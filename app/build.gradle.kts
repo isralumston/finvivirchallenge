1d4966b640ddf5ba49b4c7123f1700cbd5a26587
Israel Diaz <idiaz@lumston.com>
2024-09-03 14:03:49 -0600

    update dependency versioning to new toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -90,5 +89,6 @@ dependencies {
     /**
      * Maps for android
      *  */
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation(libs.play.services.maps)
+    implementation(libs.places)
 }
