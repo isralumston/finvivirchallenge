afe8ba9abb50472f35e264b1195c8700cc7d5393
Israel Diaz <idiaz@lumston.com>
2024-09-03 13:07:10 -0600

    initial commit: add project dependencies

---
+++ app/src/main/java/com/lumston/finvivirchallenge/framework/di/FinvivirChallengeApp.kt
@@ -0,0 +1,7 @@
+package com.lumston.finvivirchallenge.framework.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FinvivirChallengeApp: Application()
